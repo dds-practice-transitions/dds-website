bcd3bbdeccc0cf27ce54e6d93a126deba0a2f83d
Drew DeCarme <drewdecarme@gmail.com>
2024-03-26 10:44:59 -0400

    update testimonial

--- app/components/page/PageSection/PageSectionTypeTestimonialDefault.tsx
+++ app/components/page/PageSection/PageSectionTypeTestimonialDefault.tsx
@@ -10,6 +10,8 @@ import { Button } from "../../inputs/Button";
 import { Icon } from "../../display/Icon";
 import { ArrowLeft, ArrowRight, Quote } from "@icon-park/react";
 import { Avatar } from "../../display/Avatar";
+import { Responsive } from "../../../utils/Responsive";
+import { AvatarDetails } from "../../display/Avatar/AvatarDetails";
 
 export type PageSectionTypeTestimonialDefaultProps = Omit<
   JSX.IntrinsicElements["section"],
@@ -47,30 +49,63 @@ export const PageSectionTypeTestimonialDefault = forwardRef<
             {ddSubtitle}
           </PageSectionSubtitle>
         )}
-        <nav className="dirs">
-          <Button onClick={prev}>
-            <Icon DDIcon={ArrowLeft} />
-          </Button>
-          <Button onClick={next}>
-            <Icon DDIcon={ArrowRight} />
-          </Button>
-        </nav>
-        {/* <nav>
+        <div className="content">
+          <Responsive to="tablet">
+            <nav className="dirs">
+              <Button onClick={prev}>
+                <Icon DDIcon={ArrowLeft} />
+              </Button>
+              <Button onClick={next}>
+                <Icon DDIcon={ArrowRight} />
+              </Button>
+            </nav>
+          </Responsive>
+          {/* <nav>
           <ul className="quicks">
             <li>test</li>
           </ul>
         </nav> */}
-        <div className="testimonial">
-          <img
-            src={currentItem.ddChannelImg}
-            alt={currentItem.ddChannelAlt ?? "channel"}
-            className="channel"
-          />
-          <Avatar {...currentItem.ddUser} ddSize="md" />
-          <blockquote>
-            <Icon DDIcon={Quote} ddSize={48} className="i" />
-            {currentItem.ddQuote}
-          </blockquote>
+          <Responsive from="tablet">
+            <Button onClick={prev}>
+              <Icon DDIcon={ArrowLeft} />
+            </Button>
+          </Responsive>
+          <div className="testimonial">
+            <Responsive from="tablet">
+              <img
+                className="desktop-avatar"
+                src={currentItem.ddUser.ddImgSrc}
+                alt={currentItem.ddUser.ddJobTitle ?? "testimonial"}
+              />
+            </Responsive>
+            <div>
+              <img
+                src={currentItem.ddChannelImg}
+                alt={currentItem.ddChannelAlt ?? "channel"}
+                className="channel"
+              />
+              <Responsive to="tablet">
+                <Avatar {...currentItem.ddUser} ddSize="md" />
+              </Responsive>
+              <blockquote>
+                <Responsive to="tablet">
+                  <Icon DDIcon={Quote} ddSize={48} className="i" />
+                </Responsive>
+                {currentItem.ddQuote}
+              </blockquote>
+              <Responsive from="tablet">
+                <AvatarDetails
+                  {...currentItem.ddUser}
+                  className="avatar-details"
+                />
+              </Responsive>
+            </div>
+          </div>
+          <Responsive from="tablet">
+            <Button onClick={next}>
+              <Icon DDIcon={ArrowRight} />
+            </Button>
+          </Responsive>
         </div>
       </div>
     </PageSection>
