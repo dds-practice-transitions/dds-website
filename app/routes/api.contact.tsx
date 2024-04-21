222b38b237508ea4502b73064d2e344b73b6499b
Drew DeCarme <drewdecarme@gmail.com>
2024-04-21 15:29:04 -0400

    fix corm styles

--- app/routes/api.contact.tsx
+++ app/routes/api.contact.tsx
@@ -1,6 +1,10 @@
 import { ActionFunctionArgs, json } from "@remix-run/cloudflare";
 import { createAudiencefulClient } from "../lib/audienceful";
 
+export type ContactUsApiResponse = {
+  success: boolean;
+  message: string;
+};
 export const action = async ({ request, context }: ActionFunctionArgs) => {
   if (request.method !== "POST") {
     return json({ message: "Method not allowed" }, 405);
@@ -18,14 +22,16 @@ export const action = async ({ request, context }: ActionFunctionArgs) => {
     const email_address = formData.get("email_address");
 
     if (!email_address || !tags || !role) return;
-
     const person = await audiencefulClient.createPerson({
       email: email_address.toString(),
       tags: tags.concat(role.toString()) ?? [],
     });
 
-    return json({ message: "success!", person });
+    return json<ContactUsApiResponse>({ success: true, message: "success!" });
   } catch (error) {
-    return json({ message: error });
+    return json<ContactUsApiResponse>({
+      success: false,
+      message: error as string,
+    });
   }
 };
